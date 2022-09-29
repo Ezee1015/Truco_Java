3f8aab39a62510cfc09198dc08016147d406a142
Ezee1015 <routerciscoanuitti@gmail.com>
2022-09-29 15:16:09 -0300

    Modifica el nombre del fondo

--- Truco_Java/src/truco_java/Truco_Java.java
+++ Truco_Java/src/truco_java/Truco_Java.java
@@ -13,24 +13,24 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 public class Truco_Java extends JFrame{
-    
+
     public Truco_Java () throws IOException {
 
         setLayout(null);
         setDefaultCloseOperation(3);
 
         // Fondo
-        JLabel fondo = new JLabel(new ImageIcon("src/truco_java/fondos/Fondo Menu.png"));
+        JLabel fondo = new JLabel(new ImageIcon("src/truco_java/fondos/menu.png"));
         fondo.setBounds(0, 0, 500, 800);
         fondo.setVisible(true);
         add(fondo);
-        
+
         // Fondo
         JLabel icono = new JLabel(new ImageIcon(ImageIO.read(new File("src/truco_java/fondos/icono.png")).getScaledInstance(300, 100, Image.SCALE_SMOOTH)));
         icono.setBounds(100, 20, 300, 100);
         icono.setVisible(true);
         fondo.add(icono);
-        
+
         // Jugar
         JButton jugar = new JButton("Jugar");
         jugar.setBounds(100, 300, 300, 40);
@@ -49,20 +49,20 @@ public class Truco_Java extends JFrame{
             }
         });
     }
-    
+
     public static void main(String[] args) throws IOException {
-        
+
         Truco_Java menu = new Truco_Java();
         menu.setBounds(0,0,500,500);
         menu.setLocationRelativeTo(null);
         menu.setVisible(true);
-        
-        
+
+
         Music musicaFondo = new Music();
         musicaFondo.setFile("src/truco_java/musica/fondo.wav");
         musicaFondo.play();
 /*
-        
+
 */
     }
 }
