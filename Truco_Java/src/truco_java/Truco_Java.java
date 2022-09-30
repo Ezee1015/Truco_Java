1c9d31a8d6e27c3047c4351160c86c3dbf05b083
Ezee1015 <routerciscoanuitti@gmail.com>
2022-09-29 22:06:09 -0300

    Agrega un "acerca de" ventana

--- Truco_Java/src/truco_java/Truco_Java.java
+++ Truco_Java/src/truco_java/Truco_Java.java
@@ -25,22 +25,24 @@ public class Truco_Java extends JFrame{
         fondo.setVisible(true);
         add(fondo);
 
-        // Fondo
-        JLabel icono = new JLabel(new ImageIcon(ImageIO.read(new File("src/truco_java/fondos/icono.png")).getScaledInstance(300, 100, Image.SCALE_SMOOTH)));
-        icono.setBounds(100, 20, 300, 100);
-        icono.setVisible(true);
-        fondo.add(icono);
+        // Logo
+        JLabel logo = new JLabel(new ImageIcon(ImageIO.read(new File("src/truco_java/fondos/logo.png")).getScaledInstance(300, 100, Image.SCALE_SMOOTH)));
+        logo.setBounds(100, 35, 300, 100);
+        logo.setVisible(true);
+        fondo.add(logo);
 
         // Jugar
         JButton jugar = new JButton("Jugar");
-        jugar.setBounds(100, 300, 300, 40);
+        jugar.setBounds(100, 325, 300, 50);
         jugar.setVisible(true);
         fondo.add(jugar);
         jugar.addActionListener((ActionEvent e) -> {
             setVisible(false);
             InterfazJuego juego;
             try {
+                ImageIcon icono = new ImageIcon("src/truco_java/fondos/icono.png");
                 juego = new InterfazJuego(this);
+                juego.setIconImage(icono.getImage());
                 juego.setBounds(0,0,500,800);
                 juego.setLocationRelativeTo(null);
                 juego.setVisible(true);
@@ -48,11 +50,33 @@ public class Truco_Java extends JFrame{
                 Logger.getLogger(Truco_Java.class.getName()).log(Level.SEVERE, null, ex);
             }
         });
+
+        // Acerca De
+        JButton acercaBoton = new JButton("Acerca del Juego");
+        acercaBoton.setBounds(100, 400, 300, 50);
+        acercaBoton.setVisible(true);
+        fondo.add(acercaBoton);
+        acercaBoton.addActionListener((ActionEvent e) -> {
+            setVisible(false);
+            AcercaDe acerca;
+            ImageIcon icono = new ImageIcon("src/truco_java/fondos/icono.png");
+            try {
+                acerca = new AcercaDe(this);
+                acerca.setIconImage(icono.getImage());
+                acerca.setBounds(0,0,500,500);
+                acerca.setLocationRelativeTo(null);
+                acerca.setVisible(true);
+            } catch (IOException ex) {
+                Logger.getLogger(Truco_Java.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        });
     }
 
     public static void main(String[] args) throws IOException {
+        ImageIcon icono = new ImageIcon("src/ahorcadomalvinas/images/icono.png");
 
         Truco_Java menu = new Truco_Java();
+        menu.setIconImage(icono.getImage());
         menu.setBounds(0,0,500,500);
         menu.setLocationRelativeTo(null);
         menu.setVisible(true);
