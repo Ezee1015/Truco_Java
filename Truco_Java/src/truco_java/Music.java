6bd8a32ca10d642cd861c2b94143d10fe7be6b01
LeoEDS <leonardodasilva1015@gmail.com>
2022-10-17 14:26:10 -0300

    Mejora indentasión y elimina comentario

--- Truco_Java/src/truco_java/Music.java
+++ Truco_Java/src/truco_java/Music.java
@@ -9,51 +9,50 @@ import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.JOptionPane;
 
-    //Permite la utilizacion de musica en hardcore
-    public class Music {
-        Clip clip;
-        AudioInputStream sound;
+public class Music {
+    Clip clip;
+    AudioInputStream sound;
 
-        public void setFile(String soundFileName) {
-            if(!Truco_Java.musica.isSelected())
-                return;
+    public void setFile(String soundFileName) {
+        if(!Truco_Java.musica.isSelected())
+            return;
 
-            try {
-                File file = new File(soundFileName);
-                sound = AudioSystem.getAudioInputStream(file);
-                clip = AudioSystem.getClip();
-                clip.open(sound);
-                clip.loop(50);
-            } catch (IOException | LineUnavailableException | UnsupportedAudioFileException l) {
-                JOptionPane.showMessageDialog(null, "Error con el música: " + l.getMessage());
-            }
-        }
-
-        // Lo mismo que el anterior pero modifica los parametros. Esto solo hace que se reproducta una vez
-        public void setFile(String soundFileName, int x) {
-            if(!Truco_Java.musica.isSelected())
-                return;
-
-            try {
-                File file = new File(soundFileName);
-                sound = AudioSystem.getAudioInputStream(file);
-                clip = AudioSystem.getClip();
-                clip.open(sound);
-            } catch (IOException | LineUnavailableException | UnsupportedAudioFileException l) {
-                JOptionPane.showMessageDialog(null, "Error con el sonido: " + l.getMessage());
-            }
-        }
-
-        public void play() {
-            if(!Truco_Java.musica.isSelected())
-                return;
-
-            clip.start();
-        }
-
-        public void stop() throws IOException {
-            sound.close();
-            clip.close();
-            clip.stop();
+        try {
+            File file = new File(soundFileName);
+            sound = AudioSystem.getAudioInputStream(file);
+            clip = AudioSystem.getClip();
+            clip.open(sound);
+            clip.loop(50);
+        } catch (IOException | LineUnavailableException | UnsupportedAudioFileException l) {
+            JOptionPane.showMessageDialog(null, "Error con el música: " + l.getMessage());
         }
     }
+
+    // Lo mismo que el anterior pero modifica los parametros. Esto solo hace que se reproducta una vez
+    public void setFile(String soundFileName, int x) {
+        if(!Truco_Java.musica.isSelected())
+            return;
+
+        try {
+            File file = new File(soundFileName);
+            sound = AudioSystem.getAudioInputStream(file);
+            clip = AudioSystem.getClip();
+            clip.open(sound);
+        } catch (IOException | LineUnavailableException | UnsupportedAudioFileException l) {
+            JOptionPane.showMessageDialog(null, "Error con el sonido: " + l.getMessage());
+        }
+    }
+
+    public void play() {
+        if(!Truco_Java.musica.isSelected())
+            return;
+
+        clip.start();
+    }
+
+    public void stop() throws IOException {
+        sound.close();
+        clip.close();
+        clip.stop();
+    }
+}
