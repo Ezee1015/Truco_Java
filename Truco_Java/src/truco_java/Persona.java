e53afa282de8ade2eaf4046294342cd291cf96ee
LeoEDS <leonardodasilva1015@gmail.com>
2022-10-17 14:25:27 -0300

    Elimina espacio

--- Truco_Java/src/truco_java/Persona.java
+++ Truco_Java/src/truco_java/Persona.java
@@ -8,7 +8,6 @@ public class Persona extends Jugador {
         super(mano, esMano);
     }
     
-    
     public Carta tirarCarta(int pos){
         Carta aTirar = mano.get(pos);
         mano.remove(pos);
