3ab27605f90b6661e9942b8ca663864fc58febe3
Ezee1015 <routerciscoanuitti@gmail.com>
2022-10-20 15:24:14 -0300

    Agrega imports al commit anterior

--- Truco_Java/src/truco_java/Music.java
+++ Truco_Java/src/truco_java/Music.java
@@ -1,7 +1,9 @@
 package truco_java;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Timer;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
