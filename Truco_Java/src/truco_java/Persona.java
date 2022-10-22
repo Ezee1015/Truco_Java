0b183b6aacfa2f24b05df42243bb367104df257e
Ezee1015 <routerciscoanuitti@gmail.com>
2022-10-22 18:20:49 -0300

    Mejora el sonido de los mensajes

--- Truco_Java/src/truco_java/Persona.java
+++ Truco_Java/src/truco_java/Persona.java
@@ -7,7 +7,7 @@ public class Persona extends Jugador {
     public Persona(ArrayList<Carta> mano, boolean esMano) {
         super(mano, esMano);
     }
-    
+
     public Carta tirarCarta(int pos){
         Carta aTirar = mano.get(pos);
         mano.remove(pos);
