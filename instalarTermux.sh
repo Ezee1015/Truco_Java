468b880ff85bcd21209b699e704fed34503f7999
Ezee1015 <routerciscoanuitti@gmail.com>
2022-10-27 08:00:56 -0300

    Agrega script de instalación para Termux

---
+++ instalarTermux.sh
@@ -0,0 +1,25 @@
+apt update && apt upgrade
+apt install xorg-server tigervnc mpv git openjdk-17 ant
+git clone https://github.com/Ezee1015/Truco_Java
+cd Truco_Java
+cp -r Truco_Java ~/Truco_Java
+echo "alias truco='~/truco.sh && exit'" >> ~/.bashrc
+echo "alias truco='~/truco.sh && exit'" >> ~/.zshrc
+
+echo '#!/bin/bash' > ~/truco.sh
+echo 'cd ~/Truco_Java' >> ~/truco.sh
+echo 'echo "¿Quiére comprobar si hay una nueva version (y descargarla)? Presione s"' >> ~/truco.sh
+echo 'read resp' >> ~/truco.sh
+echo 'if [ "$resp" == "s" ]; then' >> ~/truco.sh
+echo '  git pull' >> ~/truco.sh
+echo 'fi' >> ~/truco.sh
+echo 'pkill Xvnc' >> ~/truco.sh
+echo 'vncserver -geometry 500x800' >> ~/truco.sh
+echo 'export DISPLAY=":1"' >> ~/truco.sh
+echo 'ant run' >> ~/truco.sh
+echo 'pkill Xvnc' >> ~/truco.sh
+
+chmod +x ~/truco.sh
+
+clear
+echo "Ya se ha terminado de instalar. Por favor reinicie la sesión y ejecute 'truco'"
