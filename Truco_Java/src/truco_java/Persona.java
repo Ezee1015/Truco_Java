57d52e3fa3e7af798e97ca8aa4ea391044e0e4e6
Ezee1015 <routerciscoanuitti@gmail.com>
2022-11-22 19:37:42 -0300

    Mejora la experiencia de la interfaz con el usuario Fija las cartas del jugador en la posicion que fueron recibidas y soluciona un bug en el envido

--- Truco_Java/src/truco_java/Persona.java
+++ Truco_Java/src/truco_java/Persona.java
@@ -3,9 +3,18 @@ package truco_java;
 import java.util.ArrayList;
 
 public class Persona extends Jugador {
+    public int posMano[] = new int[3];
 
     public Persona(ArrayList<Carta> mano, boolean esMano) {
         super(mano, esMano);
+    }
+
+    public int[] getPosMano(){
+        return posMano;
+    }
+
+    public void setPosMano(int[] p){
+        posMano=p;
     }
 
     public Carta tirarCarta(int pos){
