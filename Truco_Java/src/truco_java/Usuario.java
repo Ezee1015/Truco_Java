211b906c9557ffc0f33f54a141d31b567aeafcf7
Ezee1015 <routerciscoanuitti@gmail.com>
2022-11-25 17:17:03 -0300

    Cierra objetos Scanner

--- Truco_Java/src/truco_java/Usuario.java
+++ Truco_Java/src/truco_java/Usuario.java
@@ -50,27 +50,27 @@ public class Usuario {
     public void setContraseña(String contraseña) {
         this.contraseña = contraseña;
     }
-    
+
     public void encriptaPuntaje(){
         puntajeAI = Truco_Java.ganadasAI;
         puntajeJugador = Truco_Java.ganadasJugador;
-        try {       
+        try {
             String datosOriginales = puntajeJugador + ";sig;" + puntajeAI;
-           
+
             encriptado = new Encriptacion().encriptar(datosOriginales, contraseña);
-           
+
         } catch (UnsupportedEncodingException | NoSuchAlgorithmException | InvalidKeyException | NoSuchPaddingException | IllegalBlockSizeException | BadPaddingException ex) {
                 JOptionPane.showMessageDialog(null, "No se pudo encriptar los puntajes: " + ex.getMessage());
                 efectos.setFile("src/truco_java/musica/botonMenu.wav", 1);
                 efectos.play();
         }
     }
-    
+
     public void guardarCambios(boolean eliminar){
         File doc = new File("puntajes.txt");
         Scanner obj;
         String aGuardar="";
-        
+
         try {
             boolean seGuardo=false;
             obj = new Scanner(doc);
@@ -89,43 +89,52 @@ public class Usuario {
                 }
                 if(obj.hasNext()) aGuardar+=";sig;";
             }
-            
+
             if(!seGuardo && !eliminar){ // Si no se guardo, lo guarda
                 if(doc.length()!=0) aGuardar+=";sig;";
                 aGuardar+=nombre+";sig;"+encriptado;
             }
-                
+            obj.close();
         } catch (FileNotFoundException | NoSuchElementException ex) {
+                JOptionPane.showMessageDialog(null, "No se pudo leer los usuarios. Intente más tarde\nError: " + ex.getMessage());
+                efectos.setFile("src/truco_java/musica/botonMenu.wav", 1);
+                efectos.play();
+                return;
         }
-        
+
         try {
             FileWriter myWriter = new FileWriter("puntajes.txt");
             myWriter.write(aGuardar);
             myWriter.close();
         } catch (IOException ex) {
-                JOptionPane.showMessageDialog(null, "No se pudo escribir los usuarios: " + ex.getMessage());
-                efectos.setFile("src/truco_java/musica/botonMenu.wav", 1);
-                efectos.play();
+            JOptionPane.showMessageDialog(null, "No se pudo escribir los usuarios: " + ex.getMessage());
+            efectos.setFile("src/truco_java/musica/botonMenu.wav", 1);
+            efectos.play();
         }
-            
+
     }
-    
+
     public static void cargarUsuarios(){
         Truco_Java.listaUsuarios.clear();
         File doc = new File("puntajes.txt");
         Scanner obj;
-        
+
         try {
             obj = new Scanner(doc);
             obj.useDelimiter(";sig;");
 
             while (obj.hasNext())
                 Truco_Java.listaUsuarios.add(new Usuario(obj.next(),obj.next()));
-            
+
+            obj.close();
         } catch (FileNotFoundException | NoSuchElementException ex) {
+            JOptionPane.showMessageDialog(null, "No se pudo leer los usuarios. Intente más tarde\nError: " + ex.getMessage());
+            efectos.setFile("src/truco_java/musica/botonMenu.wav", 1);
+            efectos.play();
+            return;
         }
     }
-    
+
     public boolean iniciarSesion(String contraseña){
         this.contraseña=contraseña;
         Scanner lector;
@@ -138,7 +147,7 @@ public class Usuario {
             return false;
         }
         lector.useDelimiter(";sig;");
-        
+
         try{
             puntajeJugador=Integer.parseInt(lector.next());
             puntajeAI=Integer.parseInt(lector.next());
@@ -146,14 +155,15 @@ public class Usuario {
             Truco_Java.ganadasJugador=puntajeJugador;
             Truco_Java.puntajeJugador.setText(Integer.toString(Truco_Java.ganadasJugador));
             Truco_Java.puntajeAI.setText(Integer.toString(Truco_Java.ganadasAI));
+            lector.close();
         } catch(java.lang.NumberFormatException err){
             JOptionPane.showMessageDialog(null, "Contraseña incorrecta. No se pudo descrifar correctamente");
             efectos.setFile("src/truco_java/musica/botonMenu.wav", 1);
             efectos.play();
             return false;
         }
-        
+
         return true;
     }
-    
+
 }
