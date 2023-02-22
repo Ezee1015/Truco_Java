b39f59c365e798680881750d20b561e04b254aa4
Ezee1015 <82286822+Ezee1015@users.noreply.github.com>
2023-02-22 14:53:43 -0300

    Agrega botón de IP Pública con advertencia

--- Truco_Java/src/truco_java/EsperaServidor.java
+++ Truco_Java/src/truco_java/EsperaServidor.java
@@ -4,10 +4,13 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Enumeration;
 
@@ -25,6 +28,8 @@ import javax.swing.text.StyledDocument;
 public class EsperaServidor extends JFrame {
     JButton atras = new JButton(new ImageIcon(ImageIO.read(new File("src/truco_java/fondos/atras.png")).getScaledInstance(50, 50, Image.SCALE_SMOOTH)));
     private static final Music efectos = new Music();
+    private boolean local = true;
+    private String publicIP;
 
   public EsperaServidor (MenuJugar menu, int puertoNum, InterfazServidor servidor) throws IOException {
         setLayout(null);
@@ -86,7 +91,7 @@ public class EsperaServidor extends JFrame {
         fondo.add(direccion);
 
         JLabel ip = new JLabel("IP:");
-        ip.setBounds(90, 200, 50, 15);
+        ip.setBounds(90, 200, 150, 15);
         ip.setFont(new Font("Arial", Font.BOLD, 15));
         ip.setForeground(Color.WHITE);
         ip.setVisible(true);
@@ -101,7 +106,7 @@ public class EsperaServidor extends JFrame {
 
         JTextPane datos = new JTextPane();
         datos.setBounds(100,180,320,65);
-        datos.setText(obtenerIPs()+"\n"+puertoNum);
+        datos.setText(IPLocal()+"\n"+puertoNum);
         datos.setForeground(Color.WHITE);
         datos.setFont(new Font("Arial", Font.BOLD, 25));
         datos.setEditable(false);
@@ -112,9 +117,53 @@ public class EsperaServidor extends JFrame {
         SimpleAttributeSet center = new SimpleAttributeSet();
         StyleConstants.setAlignment(center, StyleConstants.ALIGN_CENTER);
         doc.setParagraphAttributes(0, doc.getLength(), center, false);
+
+        JLabel advertencia = new JLabel("Debe configurar 'Port Forwarding' en el router para aceptar conexiones entrantes");
+        advertencia.setBounds(10, 250, 500, 20);
+        advertencia.setFont(new Font("Arial", Font.BOLD, 12));
+        advertencia.setForeground(Color.WHITE);
+        advertencia.setVisible(false);
+        fondo.add(advertencia);
+
+        JButton ipSwitch = new JButton(new ImageIcon(ImageIO.read(new File("src/truco_java/fondos/localtrue.png")).getScaledInstance(75, 40, Image.SCALE_SMOOTH)));
+        ipSwitch.setBounds(400, 200, 75, 40);
+        ipSwitch.setVisible(true);
+        ipSwitch.setOpaque(false);
+        ipSwitch.setContentAreaFilled(false);
+        ipSwitch.setBorderPainted(false);
+        fondo.add(ipSwitch);
+        ipSwitch.addActionListener((ActionEvent e) -> {
+              if(publicIP==null)
+                     publicIP = IPPublica();
+              try {
+                    if(local){
+                          datos.setText(publicIP+"\n"+puertoNum);
+                          ip.setText("IP Pública:");
+                          advertencia.setVisible(true);
+                    }else{
+                          datos.setText(IPLocal()+"\n"+puertoNum);
+                          ip.setText("IP:");
+                          advertencia.setVisible(false);
+                    }
+
+                    local=!local;
+                    try {
+                          ipSwitch.setIcon(new ImageIcon(ImageIO.read(new File("src/truco_java/fondos/local"+local+".png")).getScaledInstance(75, 40, Image.SCALE_SMOOTH)));
+                    } catch (Exception ex) {
+                          JOptionPane.showMessageDialog(null, "Ha sucedido un error al momento de dibujar el icono de la IP: " + ex.getMessage());
+                          efectos.setFile("src/truco_java/musica/botonMenu.wav", 1);
+                          efectos.play();
+                    }
+              } catch (Exception ex) {
+                  JOptionPane.showMessageDialog(null, "Ha sucedido un error al momento de obtener la IP Pública: " + ex.getMessage());
+                  efectos.setFile("src/truco_java/musica/botonMenu.wav", 1);
+                  efectos.play();
+              }
+
+        });
   }
 
-  private String obtenerIPs(){
+  private String IPLocal(){
         Enumeration<NetworkInterface> n;
         ArrayList<String> IPs = new ArrayList<>();
         String plantillaIP = "192.168";
@@ -133,9 +182,21 @@ public class EsperaServidor extends JFrame {
         }
 
         for (String i : IPs) {
-            if(i.substring(0,7).equals(plantillaIP))
-                  return i;
+              if(i.substring(0,7).equals(plantillaIP))
+                    return i;
         }
         return "127.0.0.1";
+  }
+
+  private static String IPPublica(){
+        String urlString = "https://ifconfig.co/ip";
+        try {
+              URL url = new URL(urlString);
+              try (BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
+                    return br.readLine();
+              }
+        } catch (Exception e) {
+              return "Desconocido";
+        }
   }
 }
