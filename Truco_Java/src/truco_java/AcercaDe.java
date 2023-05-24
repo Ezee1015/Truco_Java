7379331c30459e0d95f45abadd67f0f23b3bc24e
Ezee1015 <82286822+Ezee1015@users.noreply.github.com>
2023-05-24 14:26:08 -0300

    Code Refactor in AcercaDe
    Improves the code and translate Spanish into English

--- Truco_Java/src/truco_java/AcercaDe.java
+++ Truco_Java/src/truco_java/AcercaDe.java
@@ -16,38 +16,41 @@ import javax.swing.JLabel;
 import javax.swing.JTextPane;
 
 public class AcercaDe extends JFrame {
-    JTextPane descripcion;
-    JButton atras = new JButton(new ImageIcon(ImageIO.read(new File("src/truco_java/fondos/atras.png")).getScaledInstance(50, 50, Image.SCALE_SMOOTH)));
-    JButton adelante = new JButton(new ImageIcon(ImageIO.read(new File("src/truco_java/fondos/adelante.png")).getScaledInstance(50, 50, Image.SCALE_SMOOTH)));
-    JButton salir = new JButton(new ImageIcon(ImageIO.read(new File("src/truco_java/fondos/volverBoton.png")).getScaledInstance(220, 50, Image.SCALE_SMOOTH)));
-    private static final Music efectos = new Music();
+    private JTextPane description;
+    private JButton back = new JButton(new ImageIcon(ImageIO.read(new File("src/truco_java/fondos/atras.png")).getScaledInstance(50, 50, Image.SCALE_SMOOTH)));
+    private JButton next = new JButton(new ImageIcon(ImageIO.read(new File("src/truco_java/fondos/adelante.png")).getScaledInstance(50, 50, Image.SCALE_SMOOTH)));
+    private JButton exit = new JButton(new ImageIcon(ImageIO.read(new File("src/truco_java/fondos/volverBoton.png")).getScaledInstance(220, 50, Image.SCALE_SMOOTH)));
+    private ArrayList<String> paragraph = new ArrayList<>();
+    private static final Music efects = new Music();
 
 
   public AcercaDe (Truco_Java menu) throws IOException {
         setLayout(null);
         setDefaultCloseOperation(3);
 
-        // Fondo
-        JLabel fondo = new JLabel(new ImageIcon(ImageIO.read(new File("src/truco_java/fondos/fondo_acerca.png")).getScaledInstance(500, 500, Image.SCALE_SMOOTH)));
-        fondo.setBounds(0, 0, 500, 500);
-        fondo.setVisible(true);
-        add(fondo);
+        paragraph.add("     Te despiertas en un sofá, te levantas y miras a tu alrededor: estaba Messi hablando con Maradona compartiendo unos mates, Boris Johnson hablando por celular, Guido Kaczka sentado en una silla admirando la vista y la Roca acariciando a un carpincho.");
+        paragraph.add("     Intentas recordar qué pasó, pero nada se te viene a la mente. Boris deja el celular en la mesa y viene a hablar con vos. En su mejor esfuerzo de hablar en Español, dice que están en su quinta privada secreta en Mendoza: están perseguidos por fuerzas nazis en su último intento de regreso al poder, y este es su único lugar de escondite. Por suerte, los nazis ya están casi derrotados. Las fuerzas armadas vienen al rescate, pero tardaran 12 horas en llegar. Afortunadamente, Zelenski está volando los alrededores en un avión de emergencia de la residencia de Boris, para protegernos ante cualquier ataque enemigo.");
+        paragraph.add("     Al término de la explicación le preguntas: ¿Cómo esto puede ser verdad? ¿Cómo carajo sabés español? ¿Y no era que Maradona se la dio contra el canterito? Maradona se levanta y te dice: “Demasiadas preguntas. Acá todos sabemos hablar en Español. Si eso te parece extraño, esperá a que el carpincho te gane al truco.” Te quedás helado, quieto. Maradona te grita: “¿No vas a venir a jugar? ¿Tenés miedo?” Así que te sientas en la silla y te preparas a jugar unas rondas de truco...");
 
-        // Logo
+        JLabel backgound = new JLabel(new ImageIcon(ImageIO.read(new File("src/truco_java/fondos/fondo_acerca.png")).getScaledInstance(500, 500, Image.SCALE_SMOOTH)));
+        backgound.setBounds(0, 0, 500, 500);
+        backgound.setVisible(true);
+        add(backgound);
+
         JLabel logo = new JLabel(new ImageIcon(ImageIO.read(new File("src/truco_java/fondos/logo.png")).getScaledInstance(300, 100, Image.SCALE_SMOOTH)));
         logo.setBounds(100, 20, 300, 100);
         logo.setVisible(true);
-        fondo.add(logo);
+        backgound.add(logo);
 
-        descripcion = new JTextPane();
-        descripcion.setText("     Te despiertas en un sofá, te levantas y miras a tu alrededor: estaba Messi hablando con Maradona compartiendo unos mates, Boris Johnson hablando por celular, Guido Kaczka sentado en una silla admirando la vista y la Roca acariciando a un carpincho.");
-        descripcion.setForeground(Color.WHITE);
-        descripcion.setBounds(20,130,450,275);
-        descripcion.setFont(new Font("Serif", Font.BOLD, 18));
-        descripcion.setEditable(false);
-        descripcion.setOpaque(false);
-        descripcion.setVisible(true);
-        JScrollPane scroll = new JScrollPane(descripcion);
+        description = new JTextPane();
+        description.setText("     Te despiertas en un sofá, te levantas y miras a tu alrededor: estaba Messi hablando con Maradona compartiendo unos mates, Boris Johnson hablando por celular, Guido Kaczka sentado en una silla admirando la vista y la Roca acariciando a un carpincho.");
+        description.setForeground(Color.WHITE);
+        description.setBounds(20,130,450,275);
+        description.setFont(new Font("Serif", Font.BOLD, 18));
+        description.setEditable(false);
+        description.setOpaque(false);
+        description.setVisible(true);
+        JScrollPane scroll = new JScrollPane(description);
         scroll.setVisible(true);
         scroll.setOpaque(false);
         scroll.getViewport().setOpaque(false);
@@ -55,78 +58,64 @@ public class AcercaDe extends JFrame {
         scroll.setBorder(null);
         scroll.setViewportBorder(null);
         scroll.setBounds(20,130,450,275);
-        fondo.add(scroll);
+        backgound.add(scroll);
 
-        // Atras
-        atras.setOpaque(false);
-        atras.setContentAreaFilled(false);
-        atras.setBorderPainted(false);
-        atras.setBounds(20, 410, 50, 50);
-        atras.setVisible(true);
-        atras.setEnabled(false);
-        fondo.add(atras);
-        atras.addActionListener((ActionEvent e) -> {
-            efectos.setFile("src/truco_java/musica/tirarCarta.wav", 1);
-            efectos.play();
-            determinarDescripcion(false);
+        back.setOpaque(false);
+        back.setContentAreaFilled(false);
+        back.setBorderPainted(false);
+        back.setBounds(20, 410, 50, 50);
+        back.setVisible(true);
+        back.setEnabled(false);
+        backgound.add(back);
+        back.addActionListener((ActionEvent e) -> {
+            efects.setFile("src/truco_java/musica/tirarCarta.wav", 1);
+            efects.play();
+            updateDescription(false);
         });
 
-        // Adelante
-        adelante.setOpaque(false);
-        adelante.setContentAreaFilled(false);
-        adelante.setBorderPainted(false);
-        adelante.setBounds(430, 410, 50, 50);
-        adelante.setVisible(true);
-        adelante.setEnabled(true);
-        fondo.add(adelante);
-        adelante.addActionListener((ActionEvent e) -> {
-            efectos.setFile("src/truco_java/musica/tirarCarta.wav", 1);
-            efectos.play();
-            determinarDescripcion(true);
+        next.setOpaque(false);
+        next.setContentAreaFilled(false);
+        next.setBorderPainted(false);
+        next.setBounds(430, 410, 50, 50);
+        next.setVisible(true);
+        next.setEnabled(true);
+        backgound.add(next);
+        next.addActionListener((ActionEvent e) -> {
+            efects.setFile("src/truco_java/musica/tirarCarta.wav", 1);
+            efects.play();
+            updateDescription(true);
         });
 
-        // Salir
-        salir.setBounds(140, 410, 220, 50);
-        salir.setVisible(false);
-        salir.setOpaque(false);
-        salir.setContentAreaFilled(false);
-        salir.setBorderPainted(false);
-        fondo.add(salir);
-        salir.addActionListener((ActionEvent e) -> {
-            efectos.setFile("src/truco_java/musica/botonMenu.wav", 1);
-            efectos.play();
+        exit.setBounds(140, 410, 220, 50);
+        exit.setVisible(false);
+        exit.setOpaque(false);
+        exit.setContentAreaFilled(false);
+        exit.setBorderPainted(false);
+        backgound.add(exit);
+        exit.addActionListener((ActionEvent e) -> {
+            efects.setFile("src/truco_java/musica/botonMenu.wav", 1);
+            efects.play();
             setVisible(false);
             menu.setVisible(true);
             dispose();
         });
   }
 
-  private void determinarDescripcion(boolean siguiente) {
-        ArrayList<String> parrafo = new ArrayList<>();
-        parrafo.add("     Te despiertas en un sofá, te levantas y miras a tu alrededor: estaba Messi hablando con Maradona compartiendo unos mates, Boris Johnson hablando por celular, Guido Kaczka sentado en una silla admirando la vista y la Roca acariciando a un carpincho.");
-        parrafo.add("     Intentas recordar qué pasó, pero nada se te viene a la mente. Boris deja el celular en la mesa y viene a hablar con vos. En su mejor esfuerzo de hablar en Español, dice que están en su quinta privada secreta en Mendoza: están perseguidos por fuerzas nazis en su último intento de regreso al poder, y este es su único lugar de escondite. Por suerte, los nazis ya están casi derrotados. Las fuerzas armadas vienen al rescate, pero tardaran 12 horas en llegar. Afortunadamente, Zelenski está volando los alrededores en un avión de emergencia de la residencia de Boris, para protegernos ante cualquier ataque enemigo.");
-        parrafo.add("     Al término de la explicación le preguntas: ¿Cómo esto puede ser verdad? ¿Cómo carajo sabés español? ¿Y no era que Maradona se la dio contra el canterito? Maradona se levanta y te dice: “Demasiadas preguntas. Acá todos sabemos hablar en Español. Si eso te parece extraño, esperá a que el carpincho te gane al truco.” Te quedás helado, quieto. Maradona te grita: “¿No vas a venir a jugar? ¿Tenés miedo?” Así que te sientas en la silla y te preparas a jugar unas rondas de truco...");
+  private void updateDescription(boolean next_paragraph) {
+        int indexOfDescription = paragraph.indexOf(description.getText());
 
-        for(int i=0;i<parrafo.size();i++){
-              if(descripcion.getText().equals(parrafo.get(i))){
-                    atras.setEnabled(true);
-                    adelante.setEnabled(true);
-
-                    if(siguiente && i!=parrafo.size()-1){
-                          descripcion.setText(parrafo.get(i+1));
-                          if(i==1){
-                                adelante.setEnabled(false);
-                                salir.setVisible(true);
-                          }
-                    }
-                    if(!siguiente && i!=0){
-                          descripcion.setText(parrafo.get(i-1));
-                          if(i==parrafo.size()-2)
-                                atras.setEnabled(false);
-                    }
-
-                    break;
-              }
+        if(next_paragraph){
+              description.setText(paragraph.get(indexOfDescription+1));
+              indexOfDescription++;
+        } else {
+              description.setText(paragraph.get(indexOfDescription-1));
+              indexOfDescription--;
         }
+
+        int indexOfLastParagraph = paragraph.size()-1;
+
+        back.setEnabled(indexOfDescription==0 ? false : true);
+        next.setEnabled(indexOfDescription==indexOfLastParagraph ? false : true);
+        exit.setVisible(indexOfDescription==indexOfLastParagraph ? true : false);
   }
 }
